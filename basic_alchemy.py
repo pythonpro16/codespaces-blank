3ca09001cb6098e4a2c6ede9f036ccd93bcc0742
pythonpro16 <117440035+pythonpro16@users.noreply.github.com>
2023-11-06 11:24:46 +0000

    second commit

--- basic_alchemy.py
+++ basic_alchemy.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String
 # from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import select
 
 engine = create_engine('sqlite:///alchemy_database.db')
 Base = declarative_base()
@@ -21,7 +22,7 @@ class JobPost(Base):
     location = Column(String(50))
     description = Column(String(255))
     level = Column(String(50))
-    saralry = Column(Integer,default=10000)
+    salary = Column(Integer,default=10000)
 
 
 Base.metadata.create_all(engine)
@@ -37,17 +38,40 @@ session = Session()
 # session.add(new_user)
 # session.commit()
 
-print("\nDetails of Job Postings\n")
+# print("\nDetails of Job Postings\n")
 
-job_role =  input("Enter Job Postings role: ")
-job_location = input("Enter the location: ")
-job_description = input("Enter the job description: ")
-job_level = input("Enter the level ? fresher or experience,and how many years: ")
-job_salary = input("Enter the salary for this job: ")
+# job_role =  input("Enter Job Postings role: ")
+# job_location = input("Enter the location: ")
+# job_description = input("Enter the job description: ")
+# job_level = input("Enter the level ? fresher or experience,and how many years: ")
+# job_salary = input("Enter the salary for this job: ")
 
-new_jobpost = JobPost(role_name=job_role,location=job_location,
-    description=job_description,level=job_level, saralry=job_salary)
-session.add(new_jobpost)
-session.commit()
+# new_jobpost = JobPost(role_name=job_role,location=job_location,
+#     description=job_description,level=job_level, salary=job_salary)
+# session.add(new_jobpost)
+# session.commit()
+from sqlalchemy import column
 
+# role_name = column(JobPost.role_name)
+# salary = column(JobPost.salary)
+# columns = [JobPost.role_name, JobPost.salary]
+
+# query = select([role_name, salary])
+
+results = session.query(JobPost).all()
+
+
+for result in results:
+    # role, salary = result
+    # print(f"Role: {role}, salary:{salary}")
+    print(result.id, result.role_name, result.location, result.salary)
+
+new_result = session.query(JobPost.id, JobPost.role_name, JobPost.salary).all()
+
+for result in new_result:
+    print(result)
 session.close()
+
+
+for n in range(1000):
+    print("hello")
